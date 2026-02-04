34687995fea725bdf5f19b45227655c00bc8c0a1
zero201913 <zero201913@outlook.com>
2026-02-04 21:02:46 +0800

    Site updated: 2026-02-04 21:02:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770180760508";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770210154622";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
